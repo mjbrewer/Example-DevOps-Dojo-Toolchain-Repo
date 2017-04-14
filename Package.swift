4b51d00bfc9009068370757c94b90ab4b33ce6e1
mjbrewer <mjbrewer@us.ibm.com>
2017-04-14 14:52:01 -0500

    Updating to be kitura app

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyHello"
-)
+    name: "myFirstProject",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6)
+    ])
